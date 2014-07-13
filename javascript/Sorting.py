8ab688c2c13b51ea446d67ff4496f63b5e2cc36a
matthewrmettler <matthewrmettler@gmail.com>
2014-07-13 03:02:25 -0400

    Added bubble sort

--- javascript/Sorting.py
+++ javascript/Sorting.py
@@ -21,6 +21,11 @@ Insertion sort is a simple sorting algorithm that builds the final sorted array
 -Stable; i.e., does not change the relative order of elements with equal keys
 -In-place; i.e., only requires O(1) of additional memory space
 -Online; i.e., can sort a list as it receives it
+
+Worst-case performance: O(n^2) comparisons, swaps
+Best case performance: O(n) comparisons, O(1) swaps
+Average case performance: O(n^2) comparisons, swaps
+Worst case space complexity: O(n) total, O(1) auxiliary
 '''
 def insertion_sort(array):
 	for i in range(1, len(array)):
@@ -31,9 +36,31 @@ def insertion_sort(array):
 			j = j-1
 		array[j] = x
 
+'''
+Bubble sort, sometimes referred to as sinking sort, is a simple sorting 
+algorithm that works by repeatedly stepping through the list to be sorted, 
+comparing each pair of adjacent items and swapping them if they are in the 
+wrong order. 
+
+Worst-case performance: O(n^2)
+Best case performance: O(n)
+Average case performance: O(n^2)
+Worst case space complexity: O(1) auxiliary
+'''
+def bubble_sort(array):
+	n = len(array)
+	while n > 0:
+		new_n = 0
+		for i in range(1, n):
+			if array[i-1] > array[i]:
+				temp = array[i-1]
+				array[i-1] = array[i]
+				array[i] = temp
+				new_n = i
+		n = new_n
 
 
-test_list = [x for x in range(50, 10, -3)]
+test_list = [x for x in range(50, 10, -2)]
 print(test_list)
-insertion_sort(test_list)
+bubble_sort(test_list)
 print(test_list)
